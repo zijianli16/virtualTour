9ea32939768de6dc34191035485947cadddc4d3b
Alex Li <zijianli16@gmail.com>
2020-10-29 16:40:14 -0700

    init project

---
+++ data.js
@@ -0,0 +1,278 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-room01_dirts",
+      "name": "Room01_Dirts",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -2.807950514771427,
+        "pitch": 0.546950964339004,
+        "fov": 1.3834918748083818
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9850157473897845,
+          "pitch": 0.014431965914120326,
+          "rotation": 0,
+          "target": "2-room01_sink"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.9875888884264983,
+          "pitch": 0.5774747948526766,
+          "title": "Dirt Samples",
+          "text": "Discription goes here:&nbsp;Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.&nbsp;"
+        },
+        {
+          "yaw": -0.02391572681892029,
+          "pitch": 0.18620421504768103,
+          "title": "Samples",
+          "text": "Discription goes here:&nbsp;Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "1-room01_samples",
+      "name": "Room01_Samples",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": 2.2539633342168344,
+        "pitch": 1.0132976473766462,
+        "fov": 1.3834918748083818
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0363503970074994,
+          "pitch": 0.445336706720596,
+          "rotation": 0,
+          "target": "0-room01_dirts"
+        },
+        {
+          "yaw": -0.08944806103964531,
+          "pitch": 0.19510184853116286,
+          "rotation": 0,
+          "target": "2-room01_sink"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.0482887995547365,
+          "pitch": 0.9093130504504909,
+          "title": "Three Samples",
+          "text": "Discription goes here:&nbsp;Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        },
+        {
+          "yaw": -0.7878686702052615,
+          "pitch": 0.4639815378663368,
+          "title": "Operation Table",
+          "text": "Discription goes here: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "2-room01_sink",
+      "name": "Room01_Sink",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -2.7413008054283967,
+        "pitch": 0.21126501293331046,
+        "fov": 1.3834918748083818
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6518739527670032,
+          "pitch": 0.14602027549817187,
+          "rotation": 0,
+          "target": "0-room01_dirts"
+        },
+        {
+          "yaw": -1.870162892216845,
+          "pitch": 0.3220386674455664,
+          "rotation": 0,
+          "target": "1-room01_samples"
+        },
+        {
+          "yaw": -2.24731812845865,
+          "pitch": 0.08723079616738616,
+          "rotation": 0,
+          "target": "4-room02_rightside"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.101000401838757,
+          "pitch": 0.24833444529191695,
+          "title": "Machine",
+          "text": "Discription goes here:&nbsp;Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "3-room02_operationside",
+      "name": "Room02_operationSide",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8765195182101593,
+          "pitch": 0.2502993331103909,
+          "rotation": 0,
+          "target": "1-room01_samples"
+        },
+        {
+          "yaw": -2.061443207535925,
+          "pitch": 0.5429849080188749,
+          "rotation": 0,
+          "target": "4-room02_rightside"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.6565479498938753,
+          "pitch": 0.08227744642221779,
+          "title": "Samples",
+          "text": "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.&nbsp;"
+        },
+        {
+          "yaw": 0.27605150217843644,
+          "pitch": 0.27273705089885425,
+          "title": "Operation Table",
+          "text": "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "4-room02_rightside",
+      "name": "Room02_rightSide",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": 0.37105829916129096,
+        "pitch": 0.5936090840828641,
+        "fov": 1.3834918748083818
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.29541070429976557,
+          "pitch": 0.008774865175208646,
+          "rotation": 0,
+          "target": "3-room02_operationside"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5360967565832375,
+          "pitch": 0.7561237995898242,
+          "title": "<div style=\"font-family: &quot;Segoe UI&quot;, system-ui, &quot;Apple Color Emoji&quot;, &quot;Segoe UI Emoji&quot;, sans-serif; font-size: 14px;\">Cyanobacteria</div>",
+          "text": "Discription goes here:&nbsp;It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English."
+        }
+      ]
+    }
+  ],
+  "name": "BioLab",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
